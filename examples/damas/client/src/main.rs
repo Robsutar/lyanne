0771d0155a47b9965a3e7cea334d6f6a85f035ff
Robsutar <robsonsutar@gmail.com>
2024-05-10 13:08:26 -0300

    feat: add server tick timeout in client

--- examples/damas/client/src/main.rs
+++ examples/damas/client/src/main.rs
@@ -1,9 +1,11 @@
 use lyanne::{packets::BarPacket, transport::client};
-use std::{io, sync::Arc};
-use tokio::sync::Mutex;
+use std::{io, sync::Arc, time::Duration};
+use tokio::{sync::Mutex, time::timeout};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let tick_timeout = Duration::from_secs(10);
+
     let (client, client_mut) = client::connect("127.0.0.1:8080").await?;
     let client_mut = Arc::new(Mutex::new(client_mut));
     println!("client connected");
@@ -14,16 +16,24 @@ async fn main() -> io::Result<()> {
         async move {
             loop {
                 println!("trying receive...");
-                let tuple = client::pre_read_next_message(&client).await.unwrap();
-                let mut client_mut = client_mut.lock().await;
-                client::read_next_message(&client, &mut client_mut, tuple)
-                    .await
-                    .expect("failed to read next message");
-                println!();
+                let task = client::pre_read_next_message(&client);
+                match timeout(tick_timeout, task).await {
+                    Ok(tuple) => {
+                        let mut client_mut = client_mut.lock().await;
+                        client::read_next_message(&client, &mut client_mut, tuple.unwrap())
+                            .await
+                            .expect("failed to read next message");
+                        println!();
 
-                let message = format!("Random str: {:?}", 12);
-                let packet = BarPacket { message };
-                client_mut.connected_server.send(&packet).unwrap();
+                        let message = format!("Random str: {:?}", 12);
+                        let packet = BarPacket { message };
+                        client_mut.connected_server.send(&packet).unwrap();
+                    }
+                    Err(_) => {
+                        println!("server tick timeout, aborting connection...");
+                        break;
+                    }
+                }
             }
         }
     });
