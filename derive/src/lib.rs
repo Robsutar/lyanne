ce7c1ee0f111c99c63dc287ff6334aefe7968627
Robsutar <robsonsutar@gmail.com>
2024-05-30 16:56:07 -0300

    feat: add Packet derive macro

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -4,10 +4,29 @@ use syn::DeriveInput;
 fn impl_packet_trait(ast: DeriveInput) -> TokenStream {
     let ident = ast.ident;
 
+    let client_schedule_ident = syn::Ident::new(&format!("{}ClientSchedule", ident), ident.span());
+    let server_schedule_ident = syn::Ident::new(&format!("{}ServerSchedule", ident), ident.span());
+
     quote::quote! {
         impl Packet for #ident {
+            #[cfg(all(feature = "use_bevy", feature = "client"))]
+            fn run_client_schedule(world: &mut World) -> Result<(), bevy::ecs::world::error::TryRunScheduleError>{
+                world.try_run_schedule(#client_schedule_ident)
+            }
 
+            #[cfg(all(feature = "use_bevy", feature = "server"))]
+            fn run_server_schedule(world: &mut World) -> Result<(), bevy::ecs::world::error::TryRunScheduleError>{
+                world.try_run_schedule(#server_schedule_ident)
+            }
         }
+
+        #[cfg(all(feature = "use_bevy", feature = "client"))]
+        #[derive(bevy::ecs::schedule::ScheduleLabel,Debug, Clone, PartialEq, Eq, Hash)]
+        pub struct #client_schedule_ident;
+
+        #[cfg(all(feature = "use_bevy", feature = "server"))]
+        #[derive(bevy::ecs::schedule::ScheduleLabel,Debug, Clone, PartialEq, Eq, Hash)]
+        pub struct #server_schedule_ident;
     }
     .into()
 }
