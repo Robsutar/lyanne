9d4849a2b8f1e4a5e4f30be8ce3933bb5809d8a8
Robsutar <robsonsutar@gmail.com>
2024-06-07 14:57:35 -0300

    feat: add MessagingProperties and MessageChannel

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -1,4 +1,4 @@
-use tokio::net::UdpSocket;
+use std::time::Duration;
 
 #[cfg(feature = "client")]
 pub mod client;
@@ -6,6 +6,28 @@ pub mod client;
 #[cfg(feature = "server")]
 pub mod server;
 
-pub type Socket = UdpSocket;
+pub struct MessagingProperties {
+    pub part_limit: usize,
+    pub timeout_interpretation: Duration,
+    pub packet_loss_interpretation: Duration,
+    pub initial_next_message_part_id: u8,
+}
 
-pub const MAX_PENDING_MESSAGES_STACK: usize = 2;
+impl Default for MessagingProperties {
+    fn default() -> Self {
+        Self {
+            part_limit: 1024,
+            timeout_interpretation: Duration::from_secs(10),
+            packet_loss_interpretation: Duration::from_millis(100),
+            initial_next_message_part_id: 1,
+        }
+    }
+}
+
+pub type MessageChannelType = u8;
+pub struct MessageChannel;
+
+impl MessageChannel {
+    pub const MESSAGE_PART_CONFIRM: MessageChannelType = 0;
+    pub const MESSAGE_PART_SEND: MessageChannelType = 1;
+}
