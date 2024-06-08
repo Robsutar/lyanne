31a5f115b35ff6f522bec91b01f8516ae09ca874
Robsutar <robsonsutar@gmail.com>
2024-06-08 09:01:04 -0300

    fix: fix receiving partitioned messages

--- src/transport/mod.rs
+++ src/transport/mod.rs
@@ -16,7 +16,7 @@ pub struct MessagingProperties {
 impl Default for MessagingProperties {
     fn default() -> Self {
         Self {
-            part_limit: 1024,
+            part_limit: 896,
             timeout_interpretation: Duration::from_secs(10),
             packet_loss_interpretation: Duration::from_millis(100),
             initial_next_message_part_id: 1,
