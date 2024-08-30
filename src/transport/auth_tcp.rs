8c03d1ad799392cc8e9285c0007f940857e52215
Robsutar <robsonsutar@gmail.com>
2024-08-30 01:16:38 -0300

    fix: remove unused fields

--- src/transport/auth_tcp.rs
+++ src/transport/auth_tcp.rs
@@ -2,12 +2,10 @@ use std::net::SocketAddr;
 
 #[cfg(feature = "server")]
 pub struct AuthTcpServerProperties {
-    pub server_name: &'static str,
     pub server_addr: SocketAddr,
 }
 
 #[cfg(feature = "client")]
 pub struct AuthTcpClientProperties {
-    pub server_name: &'static str,
     pub server_addr: SocketAddr,
 }
