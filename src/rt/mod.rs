c757362a535161755758f6da216b1f843d59fe2a
Robsutar <robsonsutar@gmail.com>
2024-09-05 02:43:05 -0300

    feat: add compile error with multiple rt feature enabled

--- src/rt/mod.rs
+++ src/rt/mod.rs
@@ -1,3 +1,58 @@
+#[cfg(all(
+    feature = "rt_tokio",
+    any(
+        feature = "rt_async_std",
+        feature = "rt_smol",
+        feature = "rt_async_executor",
+        feature = "rt_bevy"
+    )
+))]
+compile_error!("feature \"rt_tokio\" can not be enabled with another rt");
+
+#[cfg(all(
+    feature = "rt_async_std",
+    any(
+        feature = "rt_tokio",
+        feature = "rt_smol",
+        feature = "rt_async_executor",
+        feature = "rt_bevy"
+    )
+))]
+compile_error!("feature \"rt_async_std\" can not be enabled with another rt");
+
+#[cfg(all(
+    feature = "rt_smol",
+    any(
+        feature = "rt_tokio",
+        feature = "rt_async_std",
+        feature = "rt_async_executor",
+        feature = "rt_bevy"
+    )
+))]
+compile_error!("feature \"rt_smol\" can not be enabled with another rt");
+
+#[cfg(all(
+    feature = "rt_async_executor",
+    any(
+        feature = "rt_tokio",
+        feature = "rt_async_std",
+        feature = "rt_smol",
+        feature = "rt_bevy"
+    )
+))]
+compile_error!("feature \"rt_async_executor\" can not be enabled with another rt");
+
+#[cfg(all(
+    feature = "rt_bevy",
+    any(
+        feature = "rt_tokio",
+        feature = "rt_async_std",
+        feature = "rt_smol",
+        feature = "rt_async_executor",
+    )
+))]
+compile_error!("feature \"rt_bevy\" can not be enabled with another rt");
+
 #[cfg(feature = "rt_tokio")]
 mod tokio;
 #[cfg(feature = "rt_tokio")]
