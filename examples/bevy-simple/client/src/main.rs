8e56fa85b42e4b6b8413000719fc7ac700b3a295
Robsutar <robsonsutar@gmail.com>
2024-09-05 02:43:05 -0300

    feat(examples/bevy-simple): add bevy example
    - Bind a server
    - Connect a client
    - Send simple Foo and Bar messages in tick.

---
+++ examples/bevy-simple/client/src/main.rs
@@ -0,0 +1,109 @@
+use bevy::time::TimePlugin;
+use bevy::{app::ScheduleRunnerPlugin, log::LogPlugin, prelude::*, tasks::futures_lite::future};
+use bevy_simple::packets::*;
+use lyanne::{client::*, packets::*, *};
+use rand::Rng;
+use std::{net::SocketAddr, sync::Arc, time::Duration};
+
+#[derive(Component)]
+struct ClientConnected {
+    client: Client,
+    tick_timer: Timer,
+}
+
+fn main() {
+    App::default()
+        .add_plugins(TaskPoolPlugin::default())
+        .add_plugins(TypeRegistrationPlugin::default())
+        .add_plugins(FrameCountPlugin::default())
+        .add_plugins(ScheduleRunnerPlugin::run_loop(Duration::from_millis(3)))
+        .add_plugins(LogPlugin::default())
+        .add_plugins(TimePlugin::default())
+        .add_systems(Startup, init)
+        .add_systems(Update, server_tick)
+        .run();
+}
+
+fn init(mut commands: Commands) {
+    let packet_registry = new_packet_registry();
+
+    let remote_addr: SocketAddr = "127.0.0.1:8822".parse().unwrap();
+    let messaging_properties = Arc::new(MessagingProperties::default());
+    let read_handler_properties = Arc::new(ReadHandlerProperties::default());
+    let client_properties = Arc::new(ClientProperties::default());
+    let authenticator_mode = AuthenticatorMode::NoCryptography(AuthenticationProperties {
+        message: SerializedPacketList::create(vec![packet_registry.serialize(&HelloPacket {
+            player_name: "Josh".to_owned(),
+        })]),
+        timeout: Duration::from_secs(10),
+    });
+
+    let connect_handle = Client::connect(
+        remote_addr,
+        Arc::new(packet_registry),
+        messaging_properties,
+        read_handler_properties,
+        client_properties,
+        authenticator_mode,
+    );
+
+    let client = future::block_on(async {
+        let connect_result = connect_handle.await;
+
+        connect_result.expect("Failed to connect client").client
+    });
+
+    println!("Client connected to {:?}", remote_addr);
+
+    commands.spawn(ClientConnected {
+        client,
+        tick_timer: Timer::new(Duration::from_millis(25), TimerMode::Repeating),
+    });
+}
+
+fn server_tick(mut query: Query<&mut ClientConnected>, time: Res<Time>) {
+    for mut client_connected in query.iter_mut() {
+        if client_connected
+            .tick_timer
+            .tick(time.delta())
+            .just_finished()
+        {
+            let client = &client_connected.client;
+            match client.tick_start() {
+                ClientTickResult::ReceivedMessage(tick_result) => {
+                    use_tick_result(&client, tick_result);
+                    inside_tick(&client);
+
+                    client.tick_after_message();
+                }
+                ClientTickResult::Disconnected => {
+                    println!(
+                        "Client disconnected, reason: {:?}",
+                        client.take_disconnect_reason().unwrap()
+                    );
+                    std::process::exit(0);
+                }
+                _ => (),
+            }
+        }
+    }
+}
+
+fn use_tick_result(_client: &Client, tick_result: ReceivedMessageClientTickResult) {
+    let message = tick_result.message.to_packet_list();
+    for deserialized_packet in message {
+        if let Ok(message_packet) = deserialized_packet.packet.downcast::<MessagePacket>() {
+            println!("Server message: {:?}", message_packet.message);
+        }
+    }
+}
+
+fn inside_tick(client: &Client) {
+    let packet = MessagePacket {
+        message: "Bar?".to_owned(),
+    };
+
+    if rand::thread_rng().gen_bool(0.1) {
+        client.send_packet(&packet);
+    }
+}
---
+++ examples/bevy-simple/server/src/main.rs
@@ -0,0 +1,125 @@
+use bevy::time::TimePlugin;
+use bevy::{app::ScheduleRunnerPlugin, log::LogPlugin, prelude::*, tasks::futures_lite::future};
+use bevy_simple::packets::*;
+use lyanne::{server::*, *};
+use rand::Rng;
+use std::{net::SocketAddr, sync::Arc, time::Duration};
+
+#[derive(Component)]
+struct ServerConnected {
+    server: Server,
+    tick_timer: Timer,
+}
+
+fn main() {
+    App::default()
+        .add_plugins(TaskPoolPlugin::default())
+        .add_plugins(TypeRegistrationPlugin::default())
+        .add_plugins(FrameCountPlugin::default())
+        .add_plugins(ScheduleRunnerPlugin::run_loop(Duration::from_millis(3)))
+        .add_plugins(LogPlugin::default())
+        .add_plugins(TimePlugin::default())
+        .add_systems(Startup, init)
+        .add_systems(Update, server_tick)
+        .run();
+}
+
+fn init(mut commands: Commands) {
+    let packet_registry = new_packet_registry();
+
+    let addr: SocketAddr = "127.0.0.1:8822".parse().unwrap();
+    let messaging_properties = Arc::new(MessagingProperties::default());
+    let read_handler_properties = Arc::new(ReadHandlerProperties::default());
+    let server_properties = Arc::new(ServerProperties::default());
+    let authenticator_mode = AuthenticatorMode::NoCryptography;
+
+    let bind_handle = Server::bind(
+        addr,
+        Arc::new(packet_registry),
+        messaging_properties,
+        read_handler_properties,
+        server_properties,
+        authenticator_mode,
+    );
+
+    let server = future::block_on(async {
+        let bind_result = bind_handle.await;
+
+        bind_result.expect("Failed to bind server").server
+    });
+
+    println!("Server bind at {:?}", addr);
+
+    commands.spawn(ServerConnected {
+        server,
+        tick_timer: Timer::new(Duration::from_millis(50), TimerMode::Repeating),
+    });
+}
+
+fn server_tick(mut query: Query<&mut ServerConnected>, time: Res<Time>) {
+    for mut server_connected in query.iter_mut() {
+        if server_connected
+            .tick_timer
+            .tick(time.delta())
+            .just_finished()
+        {
+            let server = &server_connected.server;
+            let tick_result = server.tick_start();
+
+            use_tick_result(&server, tick_result);
+            inside_tick(&server);
+
+            server.tick_end();
+        }
+    }
+}
+
+fn use_tick_result(server: &Server, tick_result: ServerTickResult) {
+    for (addr, (addr_to_auth, message)) in tick_result.to_auth {
+        if let Ok(hello_packet) = message
+            .to_packet_list()
+            .remove(0)
+            .packet
+            .downcast::<HelloPacket>()
+        {
+            println!(
+                "Authenticating client {:?}, addr: {:?}",
+                hello_packet.player_name, addr
+            );
+
+            server.authenticate(addr, addr_to_auth);
+        } else {
+            println!(
+                "Client {:?} did not sent a `HelloPacket`, it will not be authenticated",
+                addr
+            );
+        }
+    }
+
+    for (addr, reason) in tick_result.disconnected {
+        println!("Client {:?} disconnected, reason: {:?}", addr, reason);
+    }
+
+    for (addr, messages) in tick_result.received_messages {
+        for message in messages {
+            let packet_list = message.to_packet_list();
+            for deserialized_packet in packet_list {
+                if let Ok(message_packet) = deserialized_packet.packet.downcast::<MessagePacket>() {
+                    println!("Client {:?} message: {:?}", addr, message_packet.message);
+                }
+            }
+        }
+    }
+}
+
+fn inside_tick(server: &Server) {
+    let packet = MessagePacket {
+        message: "Foo!".to_owned(),
+    };
+
+    for client in server.connected_clients_iter() {
+        if rand::thread_rng().gen_bool(0.1) {
+            server.send_packet(&client, &packet);
+        }
+    }
+}
