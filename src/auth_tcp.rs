da114c1c22f88ee55a9fc7231507884690458b10
Robsutar <robsonsutar@gmail.com>
2024-09-06 14:50:36 -0300

    docs: add docs on public code of auth_tcp

--- src/auth_tcp.rs
+++ src/auth_tcp.rs
@@ -1,11 +1,15 @@
 use std::net::SocketAddr;
 
+/// Properties to bind a server with Tcp authenticator.
 #[cfg(feature = "server")]
 pub struct AuthTcpServerProperties {
+    /// Addr to bind the Tcp socket.
     pub server_addr: SocketAddr,
 }
 
+/// Properties to connect a client with Tcp authenticator.
 #[cfg(feature = "client")]
 pub struct AuthTcpClientProperties {
+    /// Addr to the server Tcp socket.
     pub server_addr: SocketAddr,
 }
