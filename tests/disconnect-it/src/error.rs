edf92d420cc9dcdf4e28bb5768707a80564584a4
Robsutar <robsonsutar@gmail.com>
2024-09-21 18:16:33 -0300

    feat(tests): add disconnect-it
    Test to handle client itself disconnection.

---
+++ tests/disconnect-it/src/error.rs
@@ -0,0 +1,30 @@
+use std::{error::Error, fmt::Display};
+
+use lyanne::{
+    client::{ConnectError, ServerDisconnectReason},
+    server::{BindError, ClientDisconnectReason, ServerDisconnectClientState},
+};
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum Errors {
+    BindFail(BindError),
+    ConnectFail(ConnectError),
+    InvalidPacketDowncast(usize),
+    UnexpectedHelloPacketName(usize),
+    UnexpectedMessageContent(usize),
+    UnexpectedServerDisconnectInfo,
+    AdditionalAuthentication,
+    ClientUnexpectedDisconnection,
+    ServerShouldBeDisconnected,
+    UnexpectedBehavior(usize),
+    DisconnectionConfirmFailedByClient(ClientDisconnectReason),
+}
+
+impl Display for Errors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for Errors {}
